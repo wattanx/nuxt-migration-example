abf911f1a266e648a16622756692f35d8ffe45bb
wattanx <wattan.dev@gmail.com>
2023-09-21 00:15:11 +0900

    fix: remove console.log

--- composables/use-target-repository.ts
+++ composables/use-target-repository.ts
@@ -1,9 +1,7 @@
-import { useContext } from '@nuxtjs/composition-api';
 import { useRuntimeConfig } from '#imports';
 
 export const useTargetRepository = () => {
   const config = useRuntimeConfig();
-  console.log(config);
 
   return `${config.targetOrganization}/${config.targetRepository}`;
 };
